62af2938817b11444732c9502cbf3ecc7b22a08a
nils865 <nils.staas0806@gmail.com>
2023-01-27 12:10:28 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<style>
+  #mainPage {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100vw;
+    height: 100vh ;
+    overflow: auto;
+  }
+</style>
+
+<div id="mainPage">
+
+</div>
