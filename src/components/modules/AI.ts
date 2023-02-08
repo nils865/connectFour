870a2c85745fe625d04fef5cbd925314c625da34
Nils <nils.staas0806@gmail.com>
2023-02-08 21:48:42 +0100

    added check for defense in ai

--- src/components/modules/AI.ts
+++ src/components/modules/AI.ts
@@ -1,5 +1,11 @@
 import type { CoinList } from "../../types";
-import { columnCount, isColumnFull, spawnCoin } from "./GameController";
+import {
+    columnCount,
+    getGamefield,
+    isColumnFull,
+    spawnCoin,
+} from "./GameController";
+import { WinDetection } from "./WinGame";
 
 export class AI {
     private columns: HTMLCollection;
@@ -9,14 +15,60 @@ export class AI {
     }
 
     public spawnCoin(): CoinList {
-        const currentColumn = Math.floor(Math.random() * columnCount);
+        if (!this.checkForDefense()) {
+            const currentColumn = Math.floor(Math.random() * columnCount);
 
-        if (isColumnFull(this.columns[currentColumn] as HTMLElement))
-            return this.spawnCoin();
-        else return spawnCoin(this.columns[currentColumn] as HTMLElement);
+            if (isColumnFull(this.columns[currentColumn] as HTMLElement))
+                return this.spawnCoin();
+            else return spawnCoin(this.columns[currentColumn] as HTMLElement);
+        }
     }
 
     // check for win
     // check for defense
+    private checkForDefense() {
+        for (let i = 0; i < columnCount; i++) {
+            const currentGamefield = this.generateGamefield(i);
+            if (currentGamefield === null) continue;
+
+            console.log(currentGamefield["slot"]);
+
+            const winDetection = new WinDetection(
+                currentGamefield["slot"],
+                currentGamefield["field"]
+            );
+
+            console.log(winDetection.ConnectedElements);
+        }
+
+        return false;
+    }
     // check for best pos
+
+    private generateGamefield(pos: number) {
+        const gamefield = getGamefield();
+
+        let top = -1;
+
+        for (let i = 0; i < gamefield[pos].children.length; i++) {
+            if (
+                !gamefield[pos].children[i].children[0].classList.contains(
+                    "coin"
+                )
+            )
+                top = i;
+        }
+
+        if (top <= -1) return null;
+
+        gamefield[pos].children[top].children[0].classList.add(
+            "coin",
+            "redCoin"
+        );
+
+        return {
+            slot: gamefield[pos].children[top] as HTMLElement,
+            field: gamefield,
+        };
+    }
 }
