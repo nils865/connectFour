c5adfb0fab7f1ab777604aae130db7031a6537eb
Nils <nils.staas0806@gmail.com>
2023-02-11 00:01:27 +0100

    made pretty

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,75 @@
 <script lang="ts">
-  import { refreshGame } from "./components/modules/GameController";
-  import { gamemode } from "./stores"
-  import Game from "./components/Game.svelte";
+	import { refreshGame } from './components/modules/GameController';
+	import { gamemode } from './stores';
+	import Game from './components/Game.svelte';
 </script>
 
-<style>
-  #mainPage {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-
-    width: 100vw;
-    height: 100vh ;
-    overflow: auto;
-  }
-
-  #mode {
-    font-size: 1.25em;
-    background-color: var(--bg);
-    color: var(--txt);
-
-    padding: 5px;
-    border: 3px solid var(--txt);
-    border-radius: 0.5em;
-    width: 5em;
-    text-align: center;
-  }
-
-  #refresh {
-    width: 40px;
-    height: 40px;
-    
-    margin-right: 1em;
-    border-radius: 1;
-
-    transition: all 0.1s ease-in-out;
-  }
-
-  #refresh:hover {
-    cursor: pointer;
-
-    background-color: #404040;
-    border-radius: 10px;
-  }
-
-  #globalHead {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-</style>
-
 <div id="mainPage">
-  <div id="globalHead">
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <img on:click={() => refreshGame()} id="refresh" src="./img/refresh.svg" alt="refresh" draggable="false">
-    <select on:change={() => refreshGame()} bind:value={$gamemode} name="mode" id="mode">
-      <option value="twoPlayer">2p</option>
-      <option value="onePlayer">1p</option>
-    </select>
-  </div>
-    <Game />
+	<div id="globalHead">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<img
+			on:click={() => refreshGame()}
+			id="refresh"
+			src="./img/refresh.svg"
+			alt="refresh"
+			draggable="false" />
+		<select
+			on:change={() => refreshGame()}
+			bind:value={$gamemode}
+			name="mode"
+			id="mode">
+			<option value="twoPlayer">2p</option>
+			<option value="onePlayer">1p</option>
+		</select>
+	</div>
+	<Game />
 </div>
+
+<style>
+	#mainPage {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+
+		width: 100vw;
+		height: 100vh;
+		overflow: auto;
+	}
+
+	#mode {
+		font-size: 1.25em;
+		background-color: var(--bg);
+		color: var(--txt);
+
+		padding: 5px;
+		border: 3px solid var(--txt);
+		border-radius: 0.5em;
+		width: 5em;
+		text-align: center;
+	}
+
+	#refresh {
+		width: 40px;
+		height: 40px;
+
+		margin-right: 1em;
+		border-radius: 1;
+
+		transition: all 0.1s ease-in-out;
+	}
+
+	#refresh:hover {
+		cursor: pointer;
+
+		background-color: #404040;
+		border-radius: 10px;
+	}
+
+	#globalHead {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -1,102 +1,109 @@
 <script lang="ts">
-    import { columnCount, rowCount, spawnCoin } from "./modules/GameController";
-    import { notification, winstate, coin, gamemode } from "../stores";
-    import { AI } from "./modules/AI";
-    import { checkForWin } from "./modules/WinGame";
+	import { columnCount, rowCount, spawnCoin } from './modules/GameController';
+	import { notification, winstate, coin, gamemode } from '../stores';
+	import { AI } from './modules/AI';
+	import { checkForWin } from './modules/WinGame';
 
-    const ai: AI = new AI();
+	const ai: AI = new AI();
 
-    function click(e: MouseEvent) {
-        if ($winstate) return;
+	function click(e: MouseEvent) {
+		if ($winstate) return;
 
-        let target = e.target as HTMLElement;
+		let target = e.target as HTMLElement;
 
-        if (target.classList.contains("coin")) target = target.parentElement as HTMLElement;
-        if (target.classList.contains("slot")) target = target.parentElement as HTMLElement;
+		if (target.classList.contains('coin'))
+			target = target.parentElement as HTMLElement;
+		if (target.classList.contains('slot'))
+			target = target.parentElement as HTMLElement;
 
-        if (target.classList.contains("column") && target.id.startsWith("column")) {
-            const coinList = spawnCoin(target);
+		if (
+			target.classList.contains('column') &&
+			target.id.startsWith('column')
+		) {
+			const coinList = spawnCoin(target);
 
-            if (coinList == null) return;
+			if (coinList == null) return;
 
-            const winArgs = checkForWin(coinList);
+			const winArgs = checkForWin(coinList);
 
-            if (!winArgs["state"] && $gamemode == "onePlayer") {
-                checkForWin(ai.spawnCoin());
-            }
-        } else return;
-        
-        if ($winstate) return;
-        else if ($coin == 'redCoin') $notification = `It's <span style="color: red">reds</span> turn`
-        else if ($coin == 'yellowCoin') $notification = `It's <span style="color: yellow">yellows</span> turn`;
-    }
+			if (!winArgs['state'] && $gamemode == 'onePlayer') {
+				checkForWin(ai.spawnCoin());
+			}
+		} else return;
+
+		if ($winstate) return;
+		else if ($coin == 'redCoin')
+			$notification = `It's <span style="color: red">reds</span> turn`;
+		else if ($coin == 'yellowCoin')
+			$notification = `It's <span style="color: yellow">yellows</span> turn`;
+	}
 </script>
 
-<style>
-    #gamefield {
-        width: fit-content;
-        height: fit-content;
-        padding: 10px;
-
-        border-style: solid;
-        border-radius: 10px;
-        border-color: var(--txt);
-
-        display: flex;
-        flex-direction: row;
-
-        z-index: 1;
-    }
-
-    .column {
-        border-left: var(--txt) 4px solid;
-        z-index: 69696969;
-    }
-
-    .column:hover {
-        background-color: #0ee9da8a;
-    }
-
-    .column:last-child {
-        border-right: var(--txt) 4px solid;
-    }
-
-    .slot {
-        width: 5em;
-        height: 5em;
-
-        z-index: 2;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .slot:not(:first-child) {
-        border-top: var(--txt) 1px solid;
-    }
-
-    .slot:last-child {
-        border-bottom: var(--txt) 4px solid;
-    }
-
-    h1 {
-        text-align: center;
-    }
-</style>
-
 <div>
-    <h1>{@html $notification}</h1>
-    <div id="gamefield">
-        {#each Array(columnCount) as _, i}
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <div on:click={click} id="column{i}" class="column">
-                {#each Array(rowCount) as _}
-                    <div class="slot">
-                        <div></div>
-                    </div>
-                {/each}
-            </div>
-        {/each}
-    </div>
+	<h1>{@html $notification}</h1>
+	<div id="gamefield">
+		{#each Array(columnCount) as _, i}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div on:click={click} id="column{i}" class="column">
+				{#each Array(rowCount) as _}
+					<div class="slot">
+						<div />
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
+
+<style>
+	#gamefield {
+		width: fit-content;
+		height: fit-content;
+		padding: 10px;
+
+		border-style: solid;
+		border-radius: 10px;
+		border-color: var(--txt);
+
+		display: flex;
+		flex-direction: row;
+
+		z-index: 1;
+	}
+
+	.column {
+		border-left: var(--txt) 4px solid;
+		z-index: 69696969;
+	}
+
+	.column:hover {
+		background-color: #0ee9da8a;
+	}
+
+	.column:last-child {
+		border-right: var(--txt) 4px solid;
+	}
+
+	.slot {
+		width: 5em;
+		height: 5em;
+
+		z-index: 2;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.slot:not(:first-child) {
+		border-top: var(--txt) 1px solid;
+	}
+
+	.slot:last-child {
+		border-bottom: var(--txt) 4px solid;
+	}
+
+	h1 {
+		text-align: center;
+	}
+</style>
